eacac167f1e282726e6957a62653c72f4617ad3d
Shathana <shathana.s@gmail.com>
2020-11-06 16:25:58 +0530

    Integrated the API for Student Activity on Parent Dashboard story-22

--- components/ConstFile.js
+++ components/ConstFile.js
@@ -1,3 +1,4 @@
 export const PARENT_UPCOMING_ASSIGNMENT = "https://bloomlms.azure-api.net/parent/v1/100/200/upcoming/assignments"
 export const PARENT_UPCOMING_LESSONS = "https://bloomlms.azure-api.net/parent/v1/300/500/upcoming/lessons"
 export const PARENT_DASHBOARD_HEADER = "https://bloomlms.azure-api.net/parent/v1/300/500/upcoming/lessons"
+export const STUDENT_ACTIVITY ="https://bloomlms.azure-api.net/parent/v1/2342342/3454454/activities";
--- pages/parent/studentActivity.js
+++ pages/parent/studentActivity.js
@@ -1,43 +1,43 @@
-import React, {Component} from 'react'
+import React, { useState, useEffect } from 'react';
+import {getInfo} from '../../services/GenericServices';
+import {STUDENT_ACTIVITY} from '../../components/ConstFile';
 
-class StudentActivity extends Component{
-    render(){
-        return(
-            <div className="card Recent-Users">
-                <div className="card-header card-headerStyle">
-                    <h5>Ann's Activity</h5>
-                </div>
-                <div className="card-block px-0 py-0">
-                    <div className="table-responsive">
-                        <table className="table">
-                            <tbody>
-                                <tr className="unread alert alert-dismissible fade show" role="alert">
-                                    <td>
-                                        <h6 className="mb-1">Ann sent Ms. Jane Doe a message </h6>
-                                    </td>
-                                    <td><a href="" className="label theme-bg2 text-white f-12" data-dismiss="alert" aria-label="Close">Clear</a>
-                                    </td>
-                                </tr>
-                                <tr className="unread alert alert-dismissible fade show" role="alert">
-                                    <td>
-                                        <h6 className="mb-1">Ann's chemistry assignment is graded </h6>
-                                    </td>
-                                    <td><a href="" className="label theme-bg2 text-white f-12" data-dismiss="alert" aria-label="Close">Clear</a>
-                                    </td>
-                                </tr>
-                                <tr className="unread alert alert-dismissible fade show" role="alert">
-                                    <td>
-                                        <h6 className="mb-1">Ann assigned Photosynthesis assignment </h6>
-                                    </td>
-                                    <td><a href="" className="label theme-bg2 text-white f-12" data-dismiss="alert" aria-label="Close">Clear</a>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+
+function  studentActivity(){
+    const [studentActivityList, setActivity] = useState([]);
+
+    useEffect(() => {
+        getInfo(STUDENT_ACTIVITY).then((data) => {
+            console.log('bloom data is : ',data);
+            setActivity(data);
+        })
+    }, [])
+
+    return(
+        <div className="card Recent-Users">
+            <div className="card-header card-headerStyle">
+                <h5>Ann's Activity</h5>
+            </div>
+            <div className="card-block px-0 py-0">
+                <div className="table-responsive">
+                    <table className="table">
+                        <tbody>
+                         {   
+                           studentActivityList&&studentActivityList.map(studentActivityItem => (
+                            <tr className="unread alert alert-dismissible fade show" role="alert">
+                                <td>
+                           <h6 className="mb-1">{studentActivityItem.message}</h6>
+                                </td>
+                                <td><a href="" className="label theme-bg2 text-white f-12" data-dismiss="alert" aria-label="Close">Clear</a>
+                                </td>
+                            </tr>
+                        ))
+                        }    
+                       </tbody>
+                    </table>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
-export default StudentActivity
+export default studentActivity
