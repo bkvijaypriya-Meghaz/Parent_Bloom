d11168239fabfadef201c99e9e949062f61f79ce
Shathana <shathana.s@gmail.com>
2020-11-06 13:09:56 +0530

    Populated the parent header with relevant information - Story 25-90% completed

--- components/ConstFile.js
+++ components/ConstFile.js
@@ -1,2 +1,3 @@
 export const PARENT_UPCOMING_ASSIGNMENT = "https://bloomlms.azure-api.net/parent/v1/100/200/upcoming/assignments"
 export const PARENT_UPCOMING_LESSONS = "https://bloomlms.azure-api.net/parent/v1/300/500/upcoming/lessons"
+export const PARENT_DASHBOARD_HEADER = "https://bloomlms.azure-api.net/parent/v1/300/500/upcoming/lessons"
--- pages/parent/dashboardHeader.js
+++ pages/parent/dashboardHeader.js
@@ -1,26 +1,43 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import { useState, useEffect } from 'react';
+import {getInfo} from '../../services/GenericServices';
+import {PARENT_DASHBOARD_HEADER} from '../../components/ConstFile';
 
-class DashboardHeader extends Component{
+function DashboardHeader() {
+
+    const [parentHeaderInfo,setParentHeaderInfo] = useState([]);
+
+    //const studentProfile = myConstClass.studentProfile;
+
+    useEffect(() => {
+        getInfo(PARENT_DASHBOARD_HEADER).then((data) => {
+            console.log('getinfo :');
+            console.log('bloom data is : ',data);
+            setParentHeaderInfo(data);
+            
+        })
+    }, [])
+
     
-    render(){
+    
         return(
             
                 <div className="card">
                     <div className="row card-rowStyle">
                         <div className="col-xl-6 col-md-6 col-sm-12 col-xs-12">
                             <div className="card-header card-headerStyle" >
-                                <h5>Ms. Jane Doe !&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;4th Grade Science</h5>
+                                <h5>Ms. Jane Doe!  !&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;4th Grade Science</h5>
                             </div>
                         </div>
                         <div className="col-xl-6 col-md-6 col-sm-12 col-xs-12 text-right parentdashhead">
                             <div className="card-header card-headerStyle" >
-                                <h5>Ann Doe &nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;&nbsp;Student Summary</h5>
+                                <h5>Ann Doe !&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;&nbsp;Student Summary</h5>
                             </div>
                         </div>
                     </div>
                 </div>
             
         );
-    }
+    
 }
 export default DashboardHeader
