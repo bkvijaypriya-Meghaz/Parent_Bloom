151090a93fd90a800aa940d6dd0826f275b716e9
VictoryVP <victoryvp@users.noreply.github.com>
2020-11-07 11:30:13 +0530

    Integrated the API for Student Latest Grade on Parent Dashboard - Story 23

--- components/ConstFile.js
+++ components/ConstFile.js
@@ -2,3 +2,4 @@ export const PARENT_UPCOMING_ASSIGNMENT = "https://bloomlms.azure-api.net/parent
 export const PARENT_UPCOMING_LESSONS = "https://bloomlms.azure-api.net/parent/v1/300/500/upcoming/lessons"
 export const PARENT_DASHBOARD_HEADER = "https://bloomlms.azure-api.net/parent/v1/300/500/upcoming/lessons"
 export const STUDENT_ACTIVITY ="https://bloomlms.azure-api.net/parent/v1/2342342/3454454/activities";
+export const GRADE_INFORMATION = "https://bloomlms.azure-api.net/parent/v1/500/234342/grades/latest"
--- pages/parent/studentGradeInfo.js
+++ pages/parent/studentGradeInfo.js
@@ -1,73 +1,62 @@
 import React, {Component} from 'react'
+import { useState, useEffect } from 'react';
+import {getInfo} from '../../services/GenericServices';
+import {GRADE_INFORMATION} from '../../components/ConstFile';
 
-class StudentGradeInfo extends Component{
-    render(){
-        return(
-            <div className="card Recent-Users">
-                <div className="card-header card-headerStyle">
-                    <h5>Ann Latest Grade</h5>
-                </div>
-                <div className="card-block px-0 py-0">
-                    <div className="table-responsive">
-                        <table className="table">
-                            <tbody>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Class Avg</th>
-                                    <th>Ann's Grade</th>
-                                    <th>Comment</th>
-                                </tr>
+function StudentGradeInfo(){
+    const [studentGradeInfo, setStudentGradeInfo] = useState(0);
+    const [gradesList, setGradesList] = useState([]);
+    useEffect(() => {
+        getInfo(GRADE_INFORMATION).then((data) => {
+            setStudentGradeInfo(data);
+            setGradesList(data.grades);
+        })
+    }, [])
+
+    return(
+        <div className="card Recent-Users">
+            <div className="card-header card-headerStyle">
+                <h5>{studentGradeInfo.firstName} Latest Grade</h5>
+            </div>
+            <div className="card-block px-0 py-0">
+                <div className="table-responsive">
+                    <table className="table">
+                        <tbody>
+                            <tr>
+                                <th>Name</th>
+                                <th>Class Avg</th>
+                                <th>Ann's Grade</th>
+                                <th>Comment</th>
+                            </tr>
+                            {
+                            gradesList.map((gradeInfo)=>(
                                 <tr className="unread">
                                     <td>
-                                        <h6 className="mb-1">Weather Science Rock</h6>
-                                        <p className="m-0">Ann Doe's</p>
+                                        <h6 className="mb-1">{gradeInfo.name}</h6>
+                                        <p className="m-0">{studentGradeInfo.firstName} {studentGradeInfo.lastName}'s</p>
                                     </td>
                                     <td>
-                                        <h6 className="mb-1">C</h6>
-                                        <h6 className="text-muted"><i className="fas fa-circle text-c-green f-10 m-r-15"></i>14/20</h6>
+                                        <h6 className="mb-1">{gradeInfo.classAverage}</h6>
+                                        <h6 className="text-muted">
+                                            <i className="fas fa-circle text-c-green f-10 m-r-15"></i>
+                                            {gradeInfo.classAvgScore}
+                                        </h6>
                                     </td>
                                     <td>
-                                        <h6 className="mb-1">A</h6>
-                                        <h6 className="text-muted"><i className="fas fa-circle text-c-green f-10 m-r-15"></i>18/20</h6>
+                                        <h6 className="mb-1">{gradeInfo.myGrade}</h6>
+                                        <h6 className="text-muted"><i className="fas fa-circle text-c-green f-10 m-r-15"></i>{gradeInfo.myScore}</h6>
                                     </td>
-                                    <td className="table-responsive-tdStyle textcenter commenticon"><i className="ti-comment " data-toggle="tooltip" data-html="true" data-placement="bottom" title="Review the concepts from our Energy Lesson"></i></td>
+                                    <td className="table-responsive-tdStyle textcenter commenticon">
+                                        <i className="ti-comment " data-toggle="tooltip" data-html="true" data-placement="bottom" title={gradeInfo.comments[0].comment}></i>
+                                    </td>
                                 </tr>
-                                <tr className="unread">
-                                    <td>
-                                        <h6 className="mb-1">Patterns of Earth Quiz</h6>
-                                        <p className="m-0">Ann Doe's</p>
-                                    </td>
-                                    <td>
-                                        <h6 className="mb-1">B</h6>
-                                        <h6 className="text-muted"><i className="fas fa-circle text-c-green f-10 m-r-15"></i>8/10</h6>
-                                    </td>
-                                    <td>
-                                        <h6 className="mb-1">B</h6>
-                                        <h6 className="text-muted"><i className="fas fa-circle text-c-green f-10 m-r-15"></i>8/10</h6>
-                                    </td>
-                                    <td className="table-responsive-tdStyle textcenter commenticon"><i className="ti-comment " data-toggle="tooltip" data-html="true" data-placement="bottom" title="Review the concepts from our Energy Lesson"></i></td>
-                                </tr>
-                                <tr className="unread">
-                                    <td>
-                                        <h6 className="mb-1">Chemistry Assignment</h6>
-                                        <p className="m-0">Ann Doe's</p>
-                                    </td>
-                                    <td>
-                                        <h6 className="mb-1">B</h6>
-                                        <h6 className="text-muted"><i className="fas fa-circle text-c-green f-10 m-r-15"></i>4/5</h6>
-                                    </td>
-                                    <td>
-                                        <h6 className="mb-1">A</h6>
-                                        <h6 className="text-muted"><i className="fas fa-circle text-c-green f-10 m-r-15"></i>5/5</h6>
-                                    </td>
-                                    <td className="table-responsive-tdStyle textcenter commenticon"><i className="ti-comment " data-toggle="tooltip" data-html="true" data-placement="bottom" title="Review the concepts from our Energy Lesson"></i></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                                ))
+                            }
+                        </tbody>
+                    </table>
                 </div>
             </div>
-        );
-    }
+        </div>
+    )
 }
 export default StudentGradeInfo
