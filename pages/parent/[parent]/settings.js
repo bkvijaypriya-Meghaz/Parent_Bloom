53efb2859ec41960a83a0d3e2180e6dc9aa739b6
VictoryVP <victoryvp@users.noreply.github.com>
2020-11-08 17:34:49 +0530

    Update next link - Story 26

---
+++ pages/parent/[parent]/settings.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './header';
+import Footer from '../footer';
+
+function settings(props)
+{
+    return(
+        <div>
+            <Header></Header>
+                <div style={{height:'750px', textAlign:'center'}} >
+                    <div style={{paddingTop:'200px'}}>
+                        <h1>SETTINGS COMPONENT</h1>
+                    </div>
+                </div>
+            <Footer></Footer>
+       </div>
+    )
+}
+
+export default settings
+
